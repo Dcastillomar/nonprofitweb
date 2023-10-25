780bac8e2f60298a4fc5215fefa65d7b4e45c706
Dcastillomar <125750537+Dcastillomar@users.noreply.github.com>
2023-10-25 16:59:11 -0500

    update composer lock

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit0aa7148272c380d8d4d48317eb390443
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit0aa7148272c380d8d4d48317eb390443', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit0aa7148272c380d8d4d48317eb390443', 'loadClassLoader'));
@@ -32,18 +30,6 @@ class ComposerAutoloaderInit0aa7148272c380d8d4d48317eb390443
         call_user_func(\Composer\Autoload\ComposerStaticInit0aa7148272c380d8d4d48317eb390443::getInitializer($loader));
 
         $loader->register(true);
-
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit0aa7148272c380d8d4d48317eb390443::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
 
         return $loader;
     }
